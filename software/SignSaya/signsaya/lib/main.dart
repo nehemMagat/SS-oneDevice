d6bbac3f910321c705aeebc4260ee47b33939f63
melvintoledo <melvinandrewtoledo26@gmail.com>
2024-02-10 13:11:34 +0800

    Final Design Development

--- software/SignSaya/signsaya/lib/main.dart
+++ software/SignSaya/signsaya/lib/main.dart
@@ -24,183 +24,36 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          // Background Image
           Image.asset(
-            'lib/images/backgroundTranslation.png',
+            'lib/images/backgroundHome.png',
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
           ),
-          // Info Button
           Positioned(
-            top: 45,
-            right: 0,
-            child: ElevatedButton(
-              onPressed: null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF011F4B),
-                shape: const CircleBorder(),
-              ),
-              child: Image.asset(
-                'lib/images/infoButton.png',
-                width: 25,
-                height: 25,
-              ),
-            ),
-          ),
-          // Settings Button
-          Positioned(
-            top: 45,
-            left: 0,
-            child: ElevatedButton(
-              onPressed: null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF011F4B),
-                shape: const CircleBorder(),
-              ),
-              child: Image.asset(
-                'lib/images/settingsButton.png',
-                width: 25,
-                height: 25,
-              ),
-            ),
-          ),
-          // Dropdown List
-          Positioned(
-            top: 230,
-            left: 25,
-            child: Container(
-              height: 30,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white,
-              ),
-              child: DropdownButton<String>(
-                hint: const Text(
-                    "Selaaaaaaaect Languaaaaaaaagsssssssssssssssssse..."), //weird interaction para humaba yung width wtf
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
-                icon: const Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black,
-                ),
-                iconSize: 30,
-                elevation: 16,
-                underline: Container(
-                  height: 2,
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.circular(10),
-                items: const [
-                  DropdownMenuItem(
-                    value: 'English',
-                    child: Text('   English'),
-                  ),
-                  DropdownMenuItem(
-                    value: 'Filipino',
-                    child: Text('   Filipino'),
-                  ),
-                  DropdownMenuItem(
-                    value: 'Korean',
-                    child: Text('   Korean'),
-                  ),
-                  DropdownMenuItem(
-                    value: 'Japanese',
-                    child: Text('   Japanese'),
-                  ),
-                ],
-                onChanged: (String? value) {
-                  print('Selected language: $value');
-                },
-                value: 'English',
-              ),
-            ),
-          ),
-          // Another Gradient Container
-          Positioned(
-            top: 280,
+            top: 650,
             left: 0,
             right: 0,
-            child: ElevatedButton(
-              onPressed: null,
-              child: Container(
-                width: 380,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: const LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                      Color(0xFFCDFFD8),
-                      Color(0xFF94B9FF),
-                    ],
+            child: Center(
+              child: SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => TranslationPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
+                  child: Text('Get Started'),
                 ),
               ),
-            ),
-          ),
-          // Translation Image
-          Positioned(
-            top: 870,
-            left: 40,
-            child: ElevatedButton(
-              onPressed: null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF011F4B),
-                shape: const CircleBorder(),
-              ),
-              child: Image.asset(
-                'lib/images/translationHome.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-          // History Button
-          Positioned(
-            top: 870,
-            left: 290,
-            child: ElevatedButton(
-              onPressed: null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF011F4B),
-                shape: const CircleBorder(),
-              ),
-              child: Image.asset(
-                'lib/images/historyButton.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-          // Gloves Placeholder
-          Positioned(
-            top: 870,
-            left: 165,
-            child: ElevatedButton(
-              onPressed: null,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF011F4B),
-                shape: const CircleBorder(),
-              ),
-              child: Image.asset(
-                'lib/images/glovesPlaceholder.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-          // Horizontal Divider
-          const Positioned(
-            top: 850,
-            left: 0,
-            right: 0,
-            child: Divider(
-              color: Colors.white,
-              thickness: 3,
             ),
           ),
         ],
--- software/SignSaya/signsaya/lib/pages/translation_page.dart
+++ software/SignSaya/signsaya/lib/pages/translation_page.dart
@@ -5,102 +5,187 @@ class TranslationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Scaffold provides the basic structure of the visual interface
+    // Scaffold provides the basic structure ogf the visual interface
     return Scaffold(
       body: Stack(
         children: [
-          // Background container with gradient color
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [Color(0xFFCDFFD8), Color(0xFF94B9FF)],
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment:
-                  MainAxisAlignment.start, // Align text at the top
-              children: [
-                const SizedBox(height: 50), // Add spacing at the top
-
-                // Application title
-                const Text(
-                  "SignSaya",
-                  style: TextStyle(
-                    fontFamily: 'Anton',
-                    fontSize: 90,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                const SizedBox(
-                  width: 350, // Adjust the width as needed
-                  child: Text(
-                    // Application description
-                    "Bridging Communication Gaps for Deaf and Mute Filipino Travelers through mobile Filipino Sign Language interpretation",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Anton',
-                      fontSize: 15,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                // Expanded widget takes all available vertical space
-                Expanded(
-                  child: Container(),
-                ),
-
-                // Button to navigate to BtConnectPage
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to BtConnectPage
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BtConnectPage()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF011F4B),
-                    shape: const CircleBorder(),
-                  ),
-                  child: Image.asset(
-                    'lib/images/buttonImage.png',
-                    width: 300,
-                    height: 300,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                const SizedBox(height: 300), // Add spacing at the bottom
-              ],
-            ),
+          // Background Image
+          Image.asset(
+            'lib/images/backgroundImage.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
           ),
-
-          // Image at the bottom right corner
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
+          // Info Button
+          Positioned(
+            top: 45,
+            right: 0,
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF011F4B),
+                shape: const CircleBorder(),
+              ),
               child: Image.asset(
-                'lib/images/estetikBot.png',
-                width: 300, // Set the width of the image
-                height: 300, // Set the height of the image
-                fit: BoxFit.contain,
+                'lib/images/infoButton.png',
+                width: 25,
+                height: 25,
               ),
             ),
           ),
-
-          // Copyright text in the far left corner
-          const Positioned(
-            bottom: 16.0, // Adjust the top offset as needed
-            left: 16.0, // Adjust the left offset as needed
-            child: Text(
-              "© 2023 SignSaya. All Rights Reserved.",
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.black,
+          // Settings Button
+          Positioned(
+            top: 45,
+            left: 0,
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF011F4B),
+                shape: const CircleBorder(),
               ),
+              child: Image.asset(
+                'lib/images/settingsButton.png',
+                width: 25,
+                height: 25,
+              ),
+            ),
+          ),
+          // Dropdown List
+          Positioned(
+            top: 230,
+            left: 25,
+            child: Container(
+              height: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: DropdownButton<String>(
+                hint: const Text(
+                    "Selaaaaaaaect Languaaaaaaaagsssssssssssssssssse..."), //weird interaction para humaba yung width wtf
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+                icon: const Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black,
+                ),
+                iconSize: 30,
+                elevation: 16,
+                underline: Container(
+                  height: 2,
+                  color: Colors.white,
+                ),
+                borderRadius: BorderRadius.circular(10),
+                items: const [
+                  DropdownMenuItem(
+                    value: 'English',
+                    child: Text('   English'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Filipino',
+                    child: Text('   Filipino'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Korean',
+                    child: Text('   Korean'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Japanese',
+                    child: Text('   Japanese'),
+                  ),
+                ],
+                onChanged: (String? value) {
+                  print('Selected language: $value');
+                },
+                value: 'English',
+              ),
+            ),
+          ),
+          // Another Gradient Container
+          Positioned(
+            top: 280,
+            left: 0,
+            right: 0,
+            child: ElevatedButton(
+              onPressed: null,
+              child: Container(
+                width: 380,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: const LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Color(0xFFCDFFD8),
+                      Color(0xFF94B9FF),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Translation Image
+          Positioned(
+            top: 870,
+            left: 40,
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF011F4B),
+                shape: const CircleBorder(),
+              ),
+              child: Image.asset(
+                'lib/images/translationHome.png',
+                width: 50,
+                height: 50,
+              ),
+            ),
+          ),
+          // History Button
+          Positioned(
+            top: 870,
+            left: 290,
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF011F4B),
+                shape: const CircleBorder(),
+              ),
+              child: Image.asset(
+                'lib/images/historyButton.png',
+                width: 50,
+                height: 50,
+              ),
+            ),
+          ),
+          // Gloves Placeholder
+          Positioned(
+            top: 870,
+            left: 165,
+            child: ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF011F4B),
+                shape: const CircleBorder(),
+              ),
+              child: Image.asset(
+                'lib/images/glovesPlaceholder.png',
+                width: 50,
+                height: 50,
+              ),
+            ),
+          ),
+          // Horizontal Divider
+          const Positioned(
+            top: 850,
+            left: 0,
+            right: 0,
+            child: Divider(
+              color: Colors.white,
+              thickness: 3,
             ),
           ),
         ],
